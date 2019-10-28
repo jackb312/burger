073815439855ff9cf3f50b087d888898b5197f1d
Jack Brettell <jack.brettell@gmail.com>
2019-10-28 15:27:19 -0700

    inital code commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES (1, "Kahuna Burger", true);
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES (2, "Royale with cheese", true);
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES (3, "Whopper", false);
